33495471eef2da833ed782c6ba02a2beab5a4ef7
Ariel Aguilar <ariel.aguilar.ricaldez@gmail.com>
2018-12-13 05:27:23 -0400

    Initial configuration

---
+++ src/app/config.route.js
@@ -0,0 +1,21 @@
+(function() {
+    'use strict';
+    
+    angular
+        .module('app')
+        .config(appRun);
+        
+    appRun.$inject = ['$urlRouterProvider', '$stateProvider'];
+
+	function appRun($urlRouterProvider, $stateProvider) {
+	    
+	    $urlRouterProvider.otherwise('/home');
+	    
+		$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: 'index.html',
+			title: 'main'
+		});
+	}
+})();
